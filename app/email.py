cb2f0b1228cf2a552659a00356860493d277315e
felipe <felipexp1000@gmail.com>
2024-08-28 00:54:57 +0000

    Att 02

--- app/email.py
+++ app/email.py
@@ -1,40 +1,12 @@
-from threading import Thread
-from flask import current_app, render_template
-from flask_mail import Message
-from . import mail
-
 import requests
-from datetime import datetime
+from flask import current_app
 
-def send_async_email(app, msg):
-    with app.app_context():
-        mail.send(msg)
-
-
-def send_email(to, subject, template, **kwargs):
-    app = current_app._get_current_object()
-    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
-                  sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])    
-    msg.html = render_template(template + '.html', **kwargs)
-    thr = Thread(target=send_async_email, args=[app, msg])
-    thr.start()
-    return thr
-
-def send_simple_message(to, subject, newUser):
-    app = current_app._get_current_object()
-    print('Enviando mensagem (POST)...', flush=True)
-    print('URL: ' + str(app.config['API_URL']), flush=True)
-    print('api: ' + str(app.config['API_KEY']), flush=True)
-    print('from: ' + str(app.config['API_FROM']), flush=True)
-    print('to: ' + str(to), flush=True)
-    print('subject: ' + str(app.config['FLASKY_MAIL_SUBJECT_PREFIX']) + ' ' + subject, flush=True)
-    print('text: ' + "Novo usuário cadastrado: " + newUser, flush=True)
-
-    resposta = requests.post(app.config['API_URL'], 
-                             auth=("api", app.config['API_KEY']), data={"from": app.config['API_FROM'], 
-                                                                        "to": to, 
-                                                                        "subject": app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject, 
-                                                                        "text": "Novo usuário cadastrado: " + newUser})
-        
-    print('Enviando mensagem (Resposta)...' + str(resposta) + ' - ' + datetime.now().strftime("%m/%d/%Y, %H:%M:%S"), flush=True)
-    return resposta
+def send_simple_message(subject, body):
+    return requests.post(
+        f"https://api.mailgun.net/v3/{current_app.config['MAILGUN_DOMAIN']}/messages",
+        auth=("api", current_app.config['MAILGUN_API_KEY']),
+        data={"from": current_app.config['FROM_EMAIL'],
+              "to": current_app.config['TO_EMAILS'],
+              "subject": subject,
+              "text": body}
+    )
--- app/main/forms.py
+++ app/main/forms.py
@@ -1,8 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired
-
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    role = SelectField('What is your role?', choices=[
+        ('user', 'User'),
+        ('admin', 'Administrator'),
+        ('moderator', 'Moderator')
+    ])
     submit = SubmitField('Submit')
