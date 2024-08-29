ea0adfc43288f022f670ea3f7a364b9862974d07
Fesslipe <119904645+Fesslipe@users.noreply.github.com>
2024-08-28 23:14:57 -0300

    Update views.py

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,9 @@
-from flask import render_template, session, redirect, url_for, current_app
-from .. import db
-from ..models import User
-from ..email import send_email, send_simple_message
+from flask import render_template, session, redirect, url_for
 from . import main
+from .. import db
+from ..models import User, Role
 from .forms import NameForm
-
+from ..email import send_simple_message
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
@@ -12,20 +11,35 @@ def index():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.name.data).first()
         if user is None:
-            user = User(username=form.name.data)
+            role = Role.query.filter_by(name=form.role.data).first()
+            if role is None:
+                role = Role(name=form.role.data)
+                db.session.add(role)
+                db.session.commit()
+            user = User(username=form.name.data, role=role)
             db.session.add(user)
             db.session.commit()
             session['known'] = False
-            print('FLASKY_ADMIN: ' + str(current_app.config['FLASKY_ADMIN']), flush=True)
-            if current_app.config['FLASKY_ADMIN']:
-                #send_email(current_app.config['FLASKY_ADMIN'], 'New User', 'mail/new_user', user=user)
-                print('Enviando mensagem...', flush=True)
-                send_simple_message([current_app.config['FLASKY_ADMIN'], "flaskaulasweb@zohomail.com"], 'Novo usuário', form.name.data)
-                print('Mensagem enviada...', flush=True)
+
+            response = send_simple_message(
+                subject="Novo usuário cadastrado",
+                body=f"Usuário {user.username} foi cadastrado com sucesso com a função {role.name} na Aplicação."
+            )
+            if response.status_code == 200:
+                print("E-mail enviado com sucesso.")
+            else:
+                print(f"Falha no envio do e-mail: {response.status_code} - {response.text}")
         else:
             session['known'] = True
         session['name'] = form.name.data
-        return redirect(url_for('.index'))
-    return render_template('index.html',
-                           form=form, name=session.get('name'),
-                           known=session.get('known', False))
+        session['role'] = form.role.data
+        return redirect(url_for('main.index'))
+
+    users = User.query.all()
+    roles = Role.query.all()
+    total_users = len(users)
+    users_by_role = {role.name: User.query.filter_by(role=role).all() for role in roles}
+
+    return render_template('index.html', form=form, name=session.get('name'),
+                           known=session.get('known', False), users=users,
+                           total_users=total_users, users_by_role=users_by_role)
